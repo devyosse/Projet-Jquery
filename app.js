76522ed6f89c83f907c857f00b8f2b9b13d0258e
devyosse <narutoshippuden884@gmail.com>
2021-12-22 11:52:33 +0100

    projet jquery

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ let body = $('body');
 body.html(body.html() + `<div>Ajout d'une div</div>`);
 
 $(body).width("150px");
-$(body).height("150px")
+$(body).height("150px");
+
+body.html(body.html() + `<div>Ajout d'une div</div>`+ `<div>Ajout de deux div</div>` + `<div>Ajout de trois div</div>` + `<div>Ajout de quatre div</div>`)
+
